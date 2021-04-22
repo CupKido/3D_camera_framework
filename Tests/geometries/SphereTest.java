20cd6509f0ca15c0c679d91b681814aaca5d2600
saar taler <taler@g.jct.ac.il>
2021-04-22 12:44:56 +0300

    starts more tests for sphere

--- Tests/geometries/SphereTest.java
+++ Tests/geometries/SphereTest.java
@@ -3,11 +3,16 @@ package geometries;
 import org.junit.jupiter.api.Test;
 import primitives.*;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SphereTest {
 
     Sphere s0 = new Sphere(new Point3D( 0,0,0), 3);
+    Sphere s1 = new Sphere(new Point3D( 5,5,5), 5);
 
     @Test
     void getNormal() {
@@ -18,5 +23,14 @@ class SphereTest {
 
     @Test
     void findIntersections() {
+        // ray in circle
+
+        // ray on circle and goes in
+        // ray on circle and goes out
+        // ray out from circle and touches it
+        // ray out from circle and gets inside it
+        assertEquals(Arrays.asList(new Point3D(5,5,10), new Point3D(5,5,0)), s1.findIntersections(new Ray(new Point3D(5,5,11), new Vector(0,0,-1))), "ERROR: FindIntersections() does not work well.");
+        // ray out from circle and does not touch it
+
     }
 }
