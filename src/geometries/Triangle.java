2095ad0f596c344899805bfc27d539ca49f9e65d
saar taler <taler@g.jct.ac.il>
2021-05-30 22:59:33 +0300

    tag 07.1

--- src/geometries/Triangle.java
+++ src/geometries/Triangle.java
@@ -15,29 +15,30 @@ public class Triangle extends Polygon{
     @Override
     public Vector getNormal(Point3D point3D) {
 
-        try {
-            plane.getNormal(point3D);
-        }catch (Exception ex){
-            throw new IllegalArgumentException("Point not in triangle");
-        }
-        Vector v0 = vertices.get(1).subtract(vertices.get(0));
-        Vector v1 = vertices.get(2).subtract(vertices.get(0));
-        Vector v2 = point3D.subtract(vertices.get(0));
-        double dot00 = v0.dotProduct(v0);
-        double dot01 = v0.dotProduct(v1);
-        double dot02 = v0.dotProduct(v2);
-        double dot11 = v1.dotProduct(v1);
-        double dot12 = v1.dotProduct(v2);
-
-        double invDenom = 1 / (dot00 * dot11 - dot01 * dot01);
-        double u = (dot11 * dot02 - dot01 * dot12) * invDenom;
-        double v = (dot00 * dot12 - dot01 * dot02) * invDenom;
-// Check if point is in triangle
-        if((u >= 0) && (v >= 0) && (u + v < 1))
-        {
-            return plane.getNormal(point3D);
-        }
-        throw new IllegalArgumentException("Point not in triangle");
+        return plane.getNormal();
+//        try {
+//            plane.getNormal(point3D);
+//        }catch (Exception ex){
+//            throw new IllegalArgumentException("Point not in triangle");
+//        }
+//        Vector v0 = vertices.get(1).subtract(vertices.get(0));
+//        Vector v1 = vertices.get(2).subtract(vertices.get(0));
+//        Vector v2 = point3D.subtract(vertices.get(0));
+//        double dot00 = v0.dotProduct(v0);
+//        double dot01 = v0.dotProduct(v1);
+//        double dot02 = v0.dotProduct(v2);
+//        double dot11 = v1.dotProduct(v1);
+//        double dot12 = v1.dotProduct(v2);
+//
+//        double invDenom = 1 / (dot00 * dot11 - dot01 * dot01);
+//        double u = (dot11 * dot02 - dot01 * dot12) * invDenom;
+//        double v = (dot00 * dot12 - dot01 * dot02) * invDenom;
+//// Check if point is in triangle
+//        if((u >= 0) && (v >= 0) && (u + v < 1))
+//        {
+//            return plane.getNormal(point3D);
+//        }
+//        throw new IllegalArgumentException("Point not in triangle");
     }
 
     @Override
